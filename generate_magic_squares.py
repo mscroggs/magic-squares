f99432b8dad0415c5eeae9c0c1fc8976aa74caca
MScroggs <matthew.w.scroggs@gmail.com>
2016-06-18 15:55:18 +0100

    Renamed file

--- generate_magic_squares.py
+++ generate_magic_squares.py
@@ -3,8 +3,11 @@ from IPython import embed
 
 
 for i in range(1,5):
-    
+    with open("output/"+str(i),"w") as f:
+        pass
     print i
     m = all_magic(i)
     for a in m:
+        with open("output/"+str(i),"a") as f:
+            f.write(unicode(a))
         print a
